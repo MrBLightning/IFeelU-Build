1d4ea2a07ed087a4b9b5a0474ad3da964c8471c3
Barak <mr.lightning@gmail.com>
2019-02-25 16:38:03 +0200

    build 9 - bigger graph points

--- public/precache-manifest.97c823ce330e4b5e1f0d734083449711.js
+++ public/precache-manifest.97c823ce330e4b5e1f0d734083449711.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.fdfcfda2.js"
   },
   {
-    "revision": "665112ff1e85803daaba",
-    "url": "/static/js/main.665112ff.chunk.js"
+    "revision": "3f204454c75a8f65dc92",
+    "url": "/static/js/main.3f204454.chunk.js"
   },
   {
     "revision": "98944ef5f16d3ca787cf",
     "url": "/static/js/2.98944ef5.chunk.js"
   },
   {
-    "revision": "665112ff1e85803daaba",
-    "url": "/static/css/main.8114b605.chunk.css"
+    "revision": "3f204454c75a8f65dc92",
+    "url": "/static/css/main.a792c89a.chunk.css"
   },
   {
-    "revision": "65962250f2af8092e7d24536ee303c5e",
+    "revision": "8a2f8839df91a155f63d00ff7e86fc64",
     "url": "/index.html"
   }
 ];
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.78220de88ed5cdafdcd6d0c5e2858701.js"
+  "/precache-manifest.97c823ce330e4b5e1f0d734083449711.js"
 );
 
 workbox.clientsClaim();
