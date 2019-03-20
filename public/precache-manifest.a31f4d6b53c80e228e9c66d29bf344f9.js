9df243b4d2e3458f352ee6051b27d7b5d524b0d5
Barak <mr.lightning@gmail.com>
2019-03-20 11:08:31 +0200

    with registration page

---
+++ public/precache-manifest.a31f4d6b53c80e228e9c66d29bf344f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "53a180f9313206a3cb41",
+    "url": "/static/js/main.53a180f9.chunk.js"
+  },
+  {
+    "revision": "fd33cecb2b8a98e51168",
+    "url": "/static/js/2.fd33cecb.chunk.js"
+  },
+  {
+    "revision": "53a180f9313206a3cb41",
+    "url": "/static/css/main.78c22a42.chunk.css"
+  },
+  {
+    "revision": "c13988227811e107c216c16f760fe2fd",
+    "url": "/index.html"
+  }
+];
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7f00338b6d73b1759745c61ea5a8b070.js"
+  "/precache-manifest.a31f4d6b53c80e228e9c66d29bf344f9.js"
 );
 
 workbox.clientsClaim();
